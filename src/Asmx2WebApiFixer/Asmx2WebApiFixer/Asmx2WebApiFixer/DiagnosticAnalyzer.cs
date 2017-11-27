f0c22f81811d273b0cbf782f46356cac96be9cda
Maarten Merken <merken.maarten@gmail.com>
2017-11-27 17:43:12 +0100

    added asmx2webapi analyser

---
+++ src/Asmx2WebApiFixer/Asmx2WebApiFixer/Asmx2WebApiFixer/DiagnosticAnalyzer.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Asmx2WebApiFixer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class Asmx2WebApiFixerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "Asmx2WebApiFixer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var classDeclaration = (context.Node as ClassDeclarationSyntax);
+
+            if (classDeclaration != null && classDeclaration.AttributeLists.Any() &&
+                classDeclaration.AttributeLists.SelectMany(a => a.Attributes).Select(at => at.Name.ToFullString()).Any(fs=>fs == "System.Web.Script.Services.ScriptService") ||
+                classDeclaration.AttributeLists.SelectMany(a => a.Attributes).Select(at => at.Name.ToFullString()).Any(fs => fs == "WebService"))
+
+            {
+                //This is a script service
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+            }
+            return;
+        }
+    }
+}
