2ffe1c89e68cc1c54ace6ad79d20fb7eec310414
Maarten Merken <merken.maarten@gmail.com>
2017-11-30 15:29:45 +0100

    asmx 2 webapi fixer demo ready

--- src/Asmx2WebApiFixer/Asmx2WebApiFixer/Asmx2WebApiFixer/CodeFixProvider.cs
+++ src/Asmx2WebApiFixer/Asmx2WebApiFixer/Asmx2WebApiFixer/CodeFixProvider.cs
@@ -13,7 +13,8 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Rename;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.CSharp.Extensions;
-
+using System.Net.Http;
+using System.Text;
 
 namespace Asmx2WebApiFixer
 {
@@ -21,7 +22,7 @@ namespace Asmx2WebApiFixer
     public class Asmx2WebApiFixerCodeFixProvider : CodeFixProvider
     {
         private const string title = "Generate WebApi Controller";
-        private string[] webApiUsings = new String[] { "System.Net.Http","System.Web.Http" };
+        private string[] webApiUsings = new String[] { "System.Net.Http", "System.Web.Http" };
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
@@ -37,7 +38,6 @@ namespace Asmx2WebApiFixer
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-
             // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
@@ -56,7 +56,10 @@ namespace Asmx2WebApiFixer
 
         private async Task<Solution> CreateWebApi(Document document, ClassDeclarationSyntax classDeclaration, CancellationToken cancellationToken)
         {
+            var semanticModel = await document.GetSemanticModelAsync();
             var webserviceName = classDeclaration.Identifier.Text;
+            webserviceName = webserviceName.Replace("WebService", String.Empty);
+            webserviceName = webserviceName.Replace("webservice", String.Empty);
             webserviceName = webserviceName.Replace("Service", String.Empty);
             var controllerName = webserviceName.Replace("service", String.Empty);
 
@@ -68,14 +71,7 @@ namespace Asmx2WebApiFixer
             var constructors = GetConstructorsFrom(classDeclaration);
             var fields = GetFieldsFrom(classDeclaration);
             var othermembers = GetMembersFrom(classDeclaration).Except(webmethods).Except(constructors).Except(fields);
-            var controllerMethods = GetApiControllerMethods(false, webmethods);
-            //var webApiController = GenerateWebApiFrom(webmethods, othermethods);
-
-            //TODO get name of webservice
-            //TODO create api controller
-            //TODO add import statements
-            //TODO remove attributes
-            //TODO add routes based on webmethod names
+            var controllerMethods = GetApiControllerMethods(semanticModel, webmethods);
 
             var newFileTree = SyntaxFactory.CompilationUnit()
                 .WithUsings(SyntaxFactory.List<UsingDirectiveSyntax>(classUsings.Concat(additionalUsings)))
@@ -87,6 +83,7 @@ namespace Asmx2WebApiFixer
                                         .AddMembers(fields.ToArray())
                                         .AddMembers(constructors.Select(c =>
                                             c.WithIdentifier(SyntaxFactory.Identifier($"{controllerName}Controller"))).ToArray())
+                                        .AddMembers(controllerMethods.ToArray())
                                         .AddMembers(othermembers.ToArray()))))
                 .WithoutLeadingTrivia()
                 .NormalizeWhitespace();
@@ -118,17 +115,112 @@ namespace Asmx2WebApiFixer
                         SyntaxFactory.SingletonSeparatedList<AttributeSyntax>(routePrefixAttribute))));
         }
 
-        private IList<MethodDeclarationSyntax> GetApiControllerMethods(bool isHttpPost, IList<MethodDeclarationSyntax> webmethods)
+        private IList<MethodDeclarationSyntax> GetApiControllerMethods(SemanticModel semanticModel, IList<MethodDeclarationSyntax> webmethods)
         {
-            //If httpPost, add httppost method
-            //If not httpPost, check on method level
+            var apiMethods = new List<MethodDeclarationSyntax>();
 
-            //Get parameters, if none, this should be a httpget
-            //if parameters and get, add querystring parameters
-            //if parameters and post, add route parameters
+            foreach (var webmethod in webmethods)
+            {
+                bool isHttpPost = false;
+                var parameters = GetMethodParameters(webmethod);
+                var routeName = webmethod.Identifier.Text;
+                var isHttpGetMethod = routeName.ToLower().StartsWith("get");
+                var complexParameters = parameters.Where(p => IsComplexType(semanticModel.GetTypeInfo(p.Type).Type));
+                var simpleParameters = parameters.Except(complexParameters);
+                var routeAttribute = GetRouteAttributeForWebmethod(routeName, semanticModel, simpleParameters);
+                AttributeSyntax methodAttribute;
 
-            return webmethods;
+                if (complexParameters.Any())
+                    isHttpPost = true;
+
+                if (!isHttpPost && !isHttpGetMethod)
+                    isHttpPost = false;
+
+                methodAttribute = GenerateAttribute(isHttpPost ? "HttpPost" : "HttpGet");
+
+                var apiMethod = webmethod.WithAttributeLists(
+                                SyntaxFactory.List<AttributeListSyntax>(
+                                    new AttributeListSyntax[]{
+                                        SyntaxFactory.AttributeList(
+                                            SyntaxFactory.SingletonSeparatedList<AttributeSyntax>(
+                                                routeAttribute
+                                            )),
+                                        SyntaxFactory.AttributeList(
+                                            SyntaxFactory.SingletonSeparatedList<AttributeSyntax>(
+                                                methodAttribute
+                                            ))
+                                    }));
+
+                var apiParameters = new SeparatedSyntaxList<ParameterSyntax>().AddRange(parameters.Select(
+                    p =>
+                    {
+                        var isComplex = IsComplexType(semanticModel.GetTypeInfo(p.Type).Type);
+
+                        if (isComplex)
+                        {
+                            return p.WithAttributeLists(
+                                                SyntaxFactory.SingletonList<AttributeListSyntax>(
+                                                    SyntaxFactory.AttributeList(
+                                                        SyntaxFactory.SingletonSeparatedList<AttributeSyntax>(
+                                                            SyntaxFactory.Attribute(
+                                                                SyntaxFactory.IdentifierName("FromBody"))))));
+                        }
+
+                        return p;
+                    }));
+
+                apiMethod = apiMethod.WithParameterList(
+                    apiMethod.ParameterList.WithParameters(
+                        apiParameters));
+
+                apiMethods.Add(apiMethod);
+            }
+            return apiMethods;
         }
+
+        private AttributeSyntax GetRouteAttributeForWebmethod(string routeName, SemanticModel semanticModel, IEnumerable<ParameterSyntax> routeParams)
+        {
+            var parameters = new List<String>();
+
+            foreach (var routeParam in routeParams)
+            {
+                var paramName = routeParam.Identifier.Text;
+                parameters.Add($"{{{paramName}}}");
+            }
+
+            var route = $"{routeName}";
+            if (routeParams.Any())
+                route = $"{route}/{String.Join("/", parameters)}";
+
+            return SyntaxFactory.Attribute(
+                        SyntaxFactory.IdentifierName("Route"))
+                    .WithArgumentList(
+                        SyntaxFactory.AttributeArgumentList(
+                            SyntaxFactory.SingletonSeparatedList<AttributeArgumentSyntax>(
+                                SyntaxFactory.AttributeArgument(
+                                    SyntaxFactory.LiteralExpression(
+                                        SyntaxKind.StringLiteralExpression,
+                                        SyntaxFactory.Literal($"{route}"))))));
+        }
+
+        private AttributeSyntax GenerateAttribute(string attribute)
+        {
+            return SyntaxFactory.Attribute(
+                        SyntaxFactory.IdentifierName($"{attribute}"));
+        }
+
+        private bool IsComplexType(ITypeSymbol type)
+        {
+            var isValueType = type.IsValueType;
+            var isString = type.Name.ToLower() == "string";
+            return !isValueType && !isString;
+        }
+
+        private IList<ParameterSyntax> GetMethodParameters(MethodDeclarationSyntax method)
+        {
+            return method.ParameterList.Parameters.ToList();
+        }
+
 
         private IList<UsingDirectiveSyntax> GetUsingsFromClass(ClassDeclarationSyntax @class)
         {
@@ -161,14 +253,14 @@ namespace Asmx2WebApiFixer
             return webservice.Members.ToList();
         }
 
-        private bool IsHttpPost(ClassDeclarationSyntax classDeclaration, MethodDeclarationSyntax method)
+        private bool IsScriptService(ClassDeclarationSyntax classDeclaration)
         {
-            var isPostConfigured = classDeclaration.AttributeLists.ContainsAttributeInList("ScriptService");
+            return classDeclaration.AttributeLists.ContainsAttributeInList("ScriptService");
+        }
 
-            if (!isPostConfigured)
-                isPostConfigured = method.AttributeLists.ContainsAttributeInList("ScriptMethod");
-
-            return isPostConfigured;
+        private bool IsHttpPostForMethodDeclaration(MethodDeclarationSyntax method)
+        {
+            return method.AttributeLists.ContainsAttributeInList("ScriptMethod");
         }
     }
 }
