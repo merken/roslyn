c6f68eac618ab4ce26acf9a398213524a4cf4a35
Maarten Merken <merken.maarten@gmail.com>
2017-12-07 10:10:47 +0100

    changes to welfare db schema

--- src/welfare/welfare.sql/Init.sql
+++ src/welfare/welfare.sql/Init.sql
@@ -1,6 +1,6 @@
 USE [Unemployment]
 GO
-/****** Object:  Table [dbo].[HandicapType]    Script Date: 5/12/2017 12:08:17 ******/
+/****** Object:  Table [dbo].[HandicapType]    Script Date: 7/12/2017 10:08:48 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -14,7 +14,7 @@ CREATE TABLE [dbo].[HandicapType](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[MaritalStatus]    Script Date: 5/12/2017 12:08:17 ******/
+/****** Object:  Table [dbo].[MaritalStatus]    Script Date: 7/12/2017 10:08:48 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -28,7 +28,7 @@ CREATE TABLE [dbo].[MaritalStatus](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[WelfareLogic]    Script Date: 5/12/2017 12:08:18 ******/
+/****** Object:  Table [dbo].[WelfareLogic]    Script Date: 7/12/2017 10:08:48 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -48,6 +48,21 @@ CREATE TABLE [dbo].[WelfareLogic](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[WelfareScript]    Script Date: 7/12/2017 10:08:48 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[WelfareScript](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](250) NOT NULL,
+	[Middleware] [varchar](max) NOT NULL,
+ CONSTRAINT [PK_WelfareScript] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 SET IDENTITY_INSERT [dbo].[HandicapType] ON 
 GO
@@ -100,6 +115,22 @@ GO
 INSERT [dbo].[WelfareLogic] ([Id], [Month], [IdMaritalStatus], [idHandicap], [NumberOfChildren], [BrutoStart], [BrutoEnd], [Age], [Value]) VALUES (11, 4, 3, 1, 1, 1000, 2000, 18, CAST(1200.00 AS Decimal(18, 2)))
 GO
 SET IDENTITY_INSERT [dbo].[WelfareLogic] OFF
+GO
+SET IDENTITY_INSERT [dbo].[WelfareScript] ON 
+GO
+INSERT [dbo].[WelfareScript] ([Id], [Name], [Middleware]) VALUES (1, N'0 months, Unmarried, No handicap, No children, low wage, 18 or older', N'if (monthsOfUnemployment != -1 && maritalStatus == 1 && handicap == 1 && numberOfChildren == 0 && 0 < brutowage && brutowage <= 1000 && 18 <= age)
+                return 953.00M;')
+GO
+INSERT [dbo].[WelfareScript] ([Id], [Name], [Middleware]) VALUES (2, N'less than 4 months, Unmarried, No handicap, No children, high wage, 18 or older', N'if (monthsOfUnemployment < 4 && maritalStatus == 1 && handicap == 1 && numberOfChildren == 0 && 1000 < brutowage && brutowage <= 2000 && 18 <= age)
+                return 1300.00M;')
+GO
+INSERT [dbo].[WelfareScript] ([Id], [Name], [Middleware]) VALUES (3, N'more than 4 months but less than 12, Unmarried, No handicap, No children, high wage, 18 or older', N'if (monthsOfUnemployment >= 4 && monthsOfUnemployment <= 12 && maritalStatus == 1 && handicap == 1 && numberOfChildren == 0 && 1000 < brutowage && brutowage <= 2000 && 18 <= age)
+                return 1200.00M;')
+GO
+INSERT [dbo].[WelfareScript] ([Id], [Name], [Middleware]) VALUES (4, N'more than 12 months, Unmarried, No handicap, No children, high wage, 18 or older', N'if (monthsOfUnemployment >= 12 && maritalStatus == 1 && handicap == 1 && numberOfChildren == 0 && 1000 < brutowage && brutowage <= 2000 && 18 <= age)
+                return 1100.00M;')
+GO
+SET IDENTITY_INSERT [dbo].[WelfareScript] OFF
 GO
 ALTER TABLE [dbo].[WelfareLogic]  WITH CHECK ADD  CONSTRAINT [FK_WelfareLogic_HandicapType] FOREIGN KEY([idHandicap])
 REFERENCES [dbo].[HandicapType] ([Id])
